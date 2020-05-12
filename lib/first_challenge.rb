01b3e6ffa4ee46ab99e33b8f0607924b78d8ebd8
Jonathan East <jonathan_east@ymail.com>
2020-05-12 17:27:54 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,6 @@
 def first_challenge
    epic_tragedy = {
+     # Can create two key/value pairs two different ways shown below
      montague: {}, # key: value
      :capulet => {} # :key => value
    }

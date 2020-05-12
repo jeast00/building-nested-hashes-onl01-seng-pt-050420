c7cf40c1ef4eab7de60185cc3fdf013319a8db33
Jonathan East <jonathan_east@ymail.com>
2020-05-12 17:33:56 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,17 @@
 def second_challenge
   epic_tragedy = {
+    # with the arrow
     :montague => {
       #your key/value pairs here!
-      }, 
-    :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => {}
+      },
+      # without the arrow
+    capulet: {
       #your key/value pairs here!
       }}
 
-  
+
 end
